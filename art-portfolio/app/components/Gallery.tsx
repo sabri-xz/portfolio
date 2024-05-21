2eb9b2ef11563b682dbe707872ba47d98759eb3c
sab <sasalabrina@gmail.com>
2024-05-21 15:00:08 -0400

    add image and image fetching

--- art-portfolio/app/components/Gallery.tsx
+++ art-portfolio/app/components/Gallery.tsx
@@ -1,8 +1,7 @@
+'use client';
+
 import ImageCard from "./ImageCard";
 import React, { useEffect, useState } from 'react';
-import * as fs from 'fs';
-import * as path from 'path';
-import sharp from "sharp";
 
 type Image = {
   id: string;
@@ -11,63 +10,36 @@ type Image = {
   src: string;
 }
 
-async function importImages(dirPath: string): Promise<Image[]> {
-  const images: Image[] = [];
-  let i = 0;
-
-  return new Promise((resolve, reject) => {
-    fs.readdir(dirPath, (err, files) => {
-      if (err) {
-        return reject('Unable to scan directory: ' + err);
-      }
-
-      const imgFiles = files.filter(file => file.endsWith('.jpg')); // Filter image files
-
-      const imgPromises = imgFiles.map(img => {
-        const filePath = path.join(dirPath, img);
-        return sharp(filePath).metadata().then(metadata => {
-          const id = `${i++}`;
-          if (typeof metadata.height === 'number' && typeof metadata.width === 'number') {
-            // only return if we have the height and width information
-            images.push({ 
-              id: id,
-              height: metadata.height,
-              width: metadata.width,
-              src: filePath
-            });
-          }
-        });
-      });
-
-      Promise.all(imgPromises)
-        .then(() => resolve(images))
-        .catch(err => reject(err));
-    });
-  });
-}
-
 export default function Gallery() {
   const [images, setImages] = useState<Image[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadImages = async () => {
       try {
-        const imgs = await importImages('../../../imgs');
+        const response = await fetch('/api/import-images');
+        const imgs = await response.json();
         setImages(imgs);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     loadImages();
   }, []);
 
+  console.log(images);
+
   return (
     <div className="grid grid-cols-3 gap-4">
-      {images.map(image => (
-        <ImageCard src={image.src} id={image.id} height={image.height} width={image.width} />
-      ))}
+      {loading ? 
+      (<div> loading.. </div>) : (
+        images.map(image => (
+          <ImageCard src={image.src} id={image.id} height={image.height} width={image.width} />
+        ))
+      )}
     </div>
   );
-
 }
--- art-portfolio/app/page.tsx
+++ art-portfolio/app/page.tsx
@@ -1,9 +1,11 @@
+import Gallery from "./components/Gallery";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold"> Hello World! </h1>
-      <div className="grid grid-cols-3 gap-4"> what is this </div>
+      <Gallery />
+      <div className="grid grid-cols-3 gap-4"> ${process.cwd()} </div>
     </main>
   );
 }
