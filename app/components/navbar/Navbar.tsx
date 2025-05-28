3de4f3d71fb0a086ef9b087c2d76a48aabf2c43d
sab <sasalabrina@gmail.com>
2025-05-27 21:54:34 -0400

    navbar close on blur

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { CollapsibleNavbar } from "./CollapsibleNavbar";
 
-export const Navbar: React.FC<{}> = () => {const [isOpen, setIsOpen] = useState(false);
+export const Navbar: React.FC<{}> = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const navbarRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (
+                navbarRef.current &&
+                !navbarRef.current.contains(event.target as Node)
+            ) {
+                setIsOpen(false);
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener("mousedown", handleOutsideClick);
+        } else {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, [isOpen, setIsOpen]);
 
     return (
         <CollapsibleNavbar
             isOpen={isOpen}
             setIsOpen={setIsOpen}
+            navbarRef={navbarRef}
         />
     );
 };
