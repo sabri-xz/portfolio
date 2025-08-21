9ff9cc63efda4e7308490ddefce91fad03801c80
sab <sasalabrina@gmail.com>
2025-08-21 19:55:10 -0400

    stopping point

--- app/components/pages/ContactPage.tsx
+++ app/components/pages/ContactPage.tsx
@@ -8,24 +8,6 @@ import { Averia_Serif_Libre } from 'next/font/google'
 
 const averia_thic = Averia_Serif_Libre({ subsets: ['latin'], weight: ['700'] });
 
-type ContactSectionProps = {
-    githubUrl: string;
-    linkedinUrl: string;
-    email: string;
-}
-
-{/* <div className={`text-th-foreground contact-icons ${isOpen ? "open" : ""}`}>
-<Link href="https://github.com/sabri-xz" target="_blank">
-    <GithubIcon className="text-th-foreground hover:text-th-secondary" width={25} height={25} />
-</Link>
-<Link href="https://www.linkedin.com/in/xinzhi-liang/" target="_blank">
-    <LinkedInIcon className="text-th-foreground hover:text-th-secondary" width={25} height={25} />
-</Link>
-<Link href="mailto:xinzhi.liang34@gmail.com" target="_blank">
-    <GmailIcon className="text-th-foreground hover:text-th-secondary" width={25} height={25} />
-</Link>
-</div> */}
-
 export const ContactSection = () => {
     const githubUrl = 'https://github.com/sabri-xz';
     const linkedinUrl = 'https://www.linkedin.com/in/xinzhi-liang/';
