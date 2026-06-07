6edfba91c6fbf18a588ded48697053e687c69b5f
sab <sasalabrina@gmail.com>
2026-06-06 20:41:33 -0400

    hides projects section for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
-import path from 'path';
+import path from "path";
 import fs from "fs";
 import { CoverPage } from "./components";
-import { AboutPage } from './components/pages/AboutPage';
-import { ContactSection } from './components/pages/ContactPage';
-import { ProjectPage } from './components/pages/ProjectPage';
+import { AboutPage } from "./components/pages/AboutPage";
+import { ContactSection } from "./components/pages/ContactPage";
+import { ProjectPage } from "./components/pages/ProjectPage";
 
 const getInfo = async (): Promise<any[]> => {
-  const filePath = path.join(process.cwd(), 'app/data', 'info.json');
-  const jsonData = fs.readFileSync(filePath, 'utf-8');
-  const info: any = JSON.parse(jsonData);
-  return info;
+    const filePath = path.join(process.cwd(), "app/data", "info.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const info: any = JSON.parse(jsonData);
+    return info;
 };
 
 export default async function Home() {
-  const infomation: any = await getInfo();
+    const infomation: any = await getInfo();
 
-  return (
-    <>
-      <CoverPage info={ infomation }/>
-      <AboutPage info={ infomation }/>
-      <ProjectPage info={ infomation }/>
-      <ContactSection/>
-    </>
-  );
+    return (
+        <>
+            <CoverPage info={infomation} />
+            <AboutPage info={infomation} />
+            {/* <ProjectPage info={ infomation }/> */}
+            <ContactSection />
+        </>
+    );
 }
